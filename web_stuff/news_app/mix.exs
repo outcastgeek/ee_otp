98153dc74d28d9c771ae05901575ddc17debc6cc
outcastgeek <outcastgeek+git@gmail.com>
2015-03-11 17:31:42 -0500

    Switch to MySQL Driver instead of PostgreSQL

--- web_stuff/news_app/mix.exs
+++ web_stuff/news_app/mix.exs
@@ -25,7 +25,9 @@ defmodule NewsApp.Mixfile do
     [{:phoenix, "~>0.10.0"},
 		 #{:phoenix, github: "phoenixframework/phoenix", override: true},
      {:phoenix_ecto, "~> 0.1"},
-     {:postgrex, ">= 0.0.0"},
+		 #{:postgrex, "~> 0.8.0"},
+     #{:postgrex, ">= 0.0.0"},
+     {:mariaex, "~> 0.1.1"},
      {:cowboy, "~> 1.0"}]
   end
 end
