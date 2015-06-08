98c2d09cec48ab99d2e014c37f1b9e072dde4206
outcastgeek <outcastgeek+git@gmail.com>
2015-06-08 01:39:39 -0500

    Small Tweaks

--- web_stuff/modern_web/lib/modern_web/utils/crypto.ex
+++ web_stuff/modern_web/lib/modern_web/utils/crypto.ex
@@ -1,9 +1,10 @@
 defmodule Utils.Crypto do
   def hash_string(str, hash_algo \\ :sha512) do
     :crypto.hash(hash_algo, str)
-      |> :erlang.bitstring_to_list
-      |> Enum.map(&(:io_lib.format("~2.16.0b", [&1])))
-      |> List.flatten
-      |> :erlang.list_to_bitstring
+		|> Base.encode64
+		#|> :erlang.bitstring_to_list
+    #|> Enum.map(&(:io_lib.format("~2.16.0b", [&1])))
+    #|> List.flatten
+    #|> :erlang.list_to_bitstring
   end
 end
