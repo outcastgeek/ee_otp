82ba880bb6e2ab02c04cd3cb10c944c1c66b4d66
Benjamin Ebby <benjamin_ebby@optum.com>
2016-07-14 16:34:01 -0500

    Fixed Bug in Insight's possible_rand_id  for when there are no records in the database

--- insight_proj/lib/insight_proj_adhoc.ex
+++ insight_proj/lib/insight_proj_adhoc.ex
@@ -2,7 +2,11 @@ defmodule InsightProj.AdHoc do
   import Ecto.Query
   alias InsightProj.Repo
 
-  #### Proverbs
+  #####################
+  ############ Proverbs
+  #####################
+
+  ### API
 
   def proverb_by_slug(slug) do
     proverb_query
@@ -16,6 +20,7 @@ defmodule InsightProj.AdHoc do
     |> Repo.one
   end
 
+  #Checkout: http://stackoverflow.com/questions/4329396/mysql-select-10-random-rows-from-600k-rows-fast
   def one_random_proverb() do
     "vieupai_proverb"
     |> get_random_id
@@ -28,7 +33,9 @@ defmodule InsightProj.AdHoc do
     proverb_query |> Repo.all
   end
 
-  def proverb_query, do: from p in "vieupai_proverb",
+  #### Queries
+
+  defp proverb_query, do: from p in "vieupai_proverb",
     join: pm in "vieupai_proverbmedia",
     select: %{id: p.id,
               title: p.title,
@@ -37,9 +44,11 @@ defmodule InsightProj.AdHoc do
               image_url: pm.image_url}
 
   #TODO: Revisit the case for which there's no matching Record!!!
-  def random_proverb_query(rand_id), do: from pv in proverb_query, where: pv.id == ^rand_id
+  defp random_proverb_query(rand_id), do: from pv in proverb_query, where: pv.id == ^rand_id
 
-  #### Utils
+  #####################
+  ############### Utils
+  #####################
 
   defp get_random_id(tbl_name), do: tbl_name |> get_max_id |> possible_rand_id
 
@@ -48,6 +57,7 @@ defmodule InsightProj.AdHoc do
     max_id_query |> Repo.one
   end
 
+  defp possible_rand_id(:nil), do: 0
   #TODO: The :random module is deprecated so move to using :rand module when proper API is known!
   defp possible_rand_id(max_id) do
     :random.seed(:erlang.timestamp())
