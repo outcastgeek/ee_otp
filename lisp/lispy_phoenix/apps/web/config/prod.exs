948532a7c630c073c9d15af7a4e127a2d830c41d
Benjamin Ebby <benjamin_ebby@optum.com>
2016-10-03 16:43:08 -0500

    Configured Lispy Phoenix for Heroku Deployment

--- lisp/lispy_phoenix/apps/web/config/prod.exs
+++ lisp/lispy_phoenix/apps/web/config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :web, Web.API.Endpoint,
   http: [port: {:system, "PORT"}],
   #################url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "arcane-lowlands-60957.herokuapp.com", port: 443]
+  #################cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +59,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
